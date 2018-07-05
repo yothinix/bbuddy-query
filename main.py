c2f661d22bc50da2e3759c5829f57fa5c8b22359
yothinix <m@yothinix.com>
2018-07-05 21:00:47 +0700

    :recycle: Reduce step to sum amount of budgets :rocket:

--- main.py
+++ main.py
@@ -10,21 +10,16 @@ class Budget:
         if start_period > end_period:
             raise ValueError
 
-        converted = []
+        sum = 0
         for budget in self.data:
             amount_month = datetime.strptime(budget['month'], '%m/%Y')
+
             _, day_in_month = monthrange(amount_month.year, amount_month.month)
             average = budget['amount'] / day_in_month
 
             for day in range(1, day_in_month + 1):
-                converted.append({
-                    'day': datetime(amount_month.year, amount_month.month, day),
-                    'amount': average
-                })
-
-        sum = 0
-        for convert in converted:
-            if start_period <= convert['day'] <= end_period:
-                sum += convert['amount']
+                amount_date = datetime(amount_month.year, amount_month.month, day)
+                if start_period <= amount_date <= end_period:
+                    sum += average
 
         return round(sum)
